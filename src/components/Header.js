f682a5050d0c39542aaa2619a76bb281e7666ea2
dhansenaz <dhansen@allthingsdot.com>
2019-01-30 08:04:30 -0800

    added flex to header component to keep items centered and responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,10 @@ import "./Header.css";
 const Header = props => {
   return (
     <div className="header-container">
-      <img src={selfie} alt="selfie" className="selfie" />
-      <h1 className="gradient-text">My_Gradients</h1>
+      <div className="logo-circle">
+      </div>
+      <div className="gradient-text">Gradients</div>
+      
     </div>
   );
 };
